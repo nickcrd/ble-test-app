ade26cdc11c97977c35a13ed51c1e074f4795013
Nick <nc@nickcrd.dev>
2020-02-07 19:43:57 +0100

    Initial Commit

---
+++ ble-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ble-test
+//
+//  Created by nc on 07.02.20.
+//  Copyright © 2020 NICKCONTROL Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ble-testTests/ble_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  ble_testTests.swift
+//  ble-testTests
+//
+//  Created by nc on 07.02.20.
+//  Copyright © 2020 NICKCONTROL Studios. All rights reserved.
+//
+
+import XCTest
+@testable import ble_test
+
+class ble_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
